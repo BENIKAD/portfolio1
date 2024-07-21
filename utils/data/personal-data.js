abb17b9c951a270811dc6d2b13251eec0aa1946b
Benjamin Adebanjo Ikuesan <51761431+BENIKAD@users.noreply.github.com>
2024-07-21 19:39:09 +0300

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,8 @@ export const personalData = {
   name: "Benjamin Ikuesan",
   profile: '/profile.png',
   designation: "Full-Stack Developer",
-  description: "I am Benjamin Ikuesan a full-Stack developer with a solid background in mathematics. I am a quick learner with a strong self-learning attitude, constantly exploring new technologies and passionate about problem-solving. While my expertise lies in front-end development, I am a full stack enthusiast, eager to engage with all aspects of web application development. My core skill set is based on JavaScript, and I enjoy utilizing it to create innovative and accessible web solutions. I am open to job opportunities that align with my skills and interests, ready to contribute and grow within a dynamic team.",
+  description: "I am Benjamin Ikuesan a full-Stack developer with a solid background in mathematics.
+    I am a quick learner with a strong self-learning attitude, constantly exploring new technologies and passionate about problem-solving. While my expertise lies in full-stack development, I am a blockchain enthusiast, eager to engage with all aspects of web application development. My core skill set is based on JavaScript, and I enjoy utilizing it to create innovative and accessible web solutions. I am open to job opportunities that align with my skills and interests, ready to contribute and grow within a dynamic team.",
   email: 'benjaminikuesan1.com',
   phone: '+393501051705',
   phone2: '+48500597612',
