dd335122a4fae24e44fb1ad147a402c71dbf6649
Benjamin Adebanjo Ikuesan <51761431+BENIKAD@users.noreply.github.com>
2025-01-05 23:48:08 +0100

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -4,6 +4,7 @@ export const contactsData = {
     address: 'Sede Operativa Ex Campomizi Casermette, LAquila, Italy',
     github: 'https://github.com/BENIKAD',
     facebook: 'https://www.facebook.com/bendgreat123',
+    premiumchat: 'https://premium.chat/bendgreat/500703',
     linkedIn: 'https://www.linkedin.com/in/benjamin-adebanjo-ikuesan-91a573102',
     twitter: 'https://x.com/BIkuesan/',
     stackOverflow: 'https://stackoverflow.com/users/20951175/benjamin-ikuesan',
